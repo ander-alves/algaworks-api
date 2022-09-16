29196430ee230b6ca68220fc2aac6501fb726e81
andsoalves <andso.alves@gmail.com>
2022-09-15 22:31:54 -0300

    First Commit

---
+++ src/main/java/com/algaworks/algaworksapi/AlgaworksApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaworksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/algaworks/algaworksapi/MeuPrimeiroController.java
@@ -0,0 +1,15 @@
+package com.algaworks.algaworksapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+}
