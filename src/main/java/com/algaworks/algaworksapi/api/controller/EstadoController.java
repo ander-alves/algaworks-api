fcc1af04035e6eb215978c949f22d2eef2581ff2
Anderson Alves <andso.alves@gmail.com>
2022-10-07 14:14:22 -0300

    Add Controllers and service Class

--- src/main/java/com/algaworks/algaworksapi/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algaworksapi/api/controller/EstadoController.java
@@ -1,23 +1,77 @@
 package com.algaworks.algaworksapi.api.controller;
 
+import com.algaworks.algaworksapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algaworksapi.domain.model.Estado;
 import com.algaworks.algaworksapi.domain.repository.EstadoRepository;
+import com.algaworks.algaworksapi.domain.service.CadastroEstadoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/estados") //Boa pratica colocar no plural.
 public class EstadoController {
-
     @Autowired
     private EstadoRepository estadoRepository;
+    @Autowired
+    private CadastroEstadoService cadastroEstadoService;
+
 
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.findAll();
+    }
+
+    @GetMapping("/{estadoId}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
+        Optional<Estado> estado = estadoRepository.findById(estadoId);
+
+        if (estado.isPresent()) {
+            return ResponseEntity.ok(estado.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Estado adicionar(@RequestBody Estado estado ) {
+        return cadastroEstadoService.salvar(estado);
+    }
+
+    @PutMapping("/{estadoId}")
+    public ResponseEntity<Estado> atualizar(@PathVariable Long estadoId,
+                                            @RequestBody Estado estado) {
+        Estado estadoAtual = estadoRepository.findById(estadoId).orElse(null);
+
+        if (estadoAtual != null) {
+            BeanUtils.copyProperties(estado, estadoAtual, "id");
+
+            estadoAtual = cadastroEstadoService.salvar(estadoAtual);
+            return ResponseEntity.ok(estadoAtual);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @DeleteMapping("/{estadoId}")
+    public ResponseEntity<Estado> remover(@PathVariable Long estadoId) {
+        try {
+            cadastroEstadoService.excluir(estadoId);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
---
+++ src/main/java/com/algaworks/algaworksapi/domain/service/CadastroEstadoService.java
@@ -0,0 +1,38 @@
+package com.algaworks.algaworksapi.domain.service;
+
+import com.algaworks.algaworksapi.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algaworksapi.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algaworksapi.domain.model.Cozinha;
+import com.algaworks.algaworksapi.domain.model.Estado;
+import com.algaworks.algaworksapi.domain.repository.CozinhaRepository;
+import com.algaworks.algaworksapi.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Estado salvar(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    public void excluir(Long estadoId) {
+        try {
+            estadoRepository.deleteById(estadoId);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(String.format(
+                    "Não existe um cadastro de estado com código %d", estadoId
+            ));
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format(
+                    "Estado de código %d não pode ser removido, pois está em uso", estadoId
+            ));
+        }
+    }
+}
