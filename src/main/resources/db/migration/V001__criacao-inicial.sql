0963ea261a1c33ef3d68aea8ea94169c39f78246
andsoalves <andso.alves@gmail.com>
2022-10-16 20:13:19 -0300

    Commit

---
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -0,0 +1,5 @@
+ CREATE TABLE `cozinha` (
+   `id` bigint NOT NULL AUTO_INCREMENT,
+   `nome` varchar(60) NOT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/resources/db/migration/V002__cria-tabela-cidade.sql
@@ -0,0 +1,6 @@
+ CREATE TABLE cidade (
+   id bigint NOT NULL AUTO_INCREMENT,
+   nome_cidade varchar(60) NOT NULL,
+   nome_estado varchar(60) NOT NULL,
+   PRIMARY KEY (id)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
